7560472780d5ffb4c140c6badd2f38622c92575b
Mohammed Ibrahim Mahmoud <mohammedjrt@gmail.com>
2025-07-09 16:19:11 +0300

    docs: update title, description swagger api docs

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -8,10 +8,9 @@ async function bootstrap() {
   app.enableCors();
 
   const config = new DocumentBuilder()
-    .setTitle('Cats example')
-    .setDescription('The cats API description')
+    .setTitle('Household Chore Tracker')
+    .setDescription('Api Documentation for Household Chore Tracker')
     .setVersion('1.0')
-    .addTag('cats')
     .build();
   const documentFactory = () => SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('api', app, documentFactory);
