dbf32ccb9beedf90bb60cbdf52f58cd1c1bed545
Mohammed Ibrahim Mahmoud <mohammedjrt@gmail.com>
2025-07-12 00:50:13 +0300

    linting the project

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,7 +1,7 @@
+import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { AppModule } from './app.module';
-import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -9,8 +9,8 @@ async function bootstrap() {
   app.enableCors();
   app.useGlobalPipes(
     new ValidationPipe({
-      whitelist: true
-    })
+      whitelist: true,
+    }),
   );
 
   const config = new DocumentBuilder()
