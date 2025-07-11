fe6726cb03b6da0f9b5a21c333e1e57833131c28
Abdul Waheed <ab.waheed1722@gmail.com>
2025-07-12 01:39:34 +0300

    feat: jwt strategy initial updates

---
+++ backend/src/auth/strategy/jwt.strategy.ts
@@ -0,0 +1,44 @@
+import { Injectable, UnauthorizedException } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
+import { PassportStrategy } from '@nestjs/passport';
+import { ExtractJwt, Strategy } from 'passport-jwt';
+import { PrismaService } from 'src/prisma/prisma.service';
+import { AuthenticatedUser } from '../interfaces';
+
+@Injectable()
+export class JwtStrategy extends PassportStrategy(Strategy) {
+  constructor(
+    config: ConfigService,
+    private prisma: PrismaService,
+  ) {
+    super({
+      jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
+      secretOrKey: config.get('JWT_SECRET')!,
+    });
+  }
+
+  async validate(payload: {
+    sub: string;
+    email: string;
+  }): Promise<AuthenticatedUser | null> {
+    const user = await this.prisma.user.findUnique({
+      where: {
+        id: payload.sub,
+      },
+      select: {
+        id: true,
+        name: true,
+        email: true,
+        role: true,
+        createdAt: true,
+        updatedAt: true,
+      },
+    });
+
+    if (!user) {
+      throw new UnauthorizedException();
+    }
+
+    return user as AuthenticatedUser;
+  }
+}
